1f33792ebb54ffd0762c7d875e2920c5d3071e99
adesahrull <64809307+adesahrull@users.noreply.github.com>
2020-05-05 04:02:30 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I'm sorry for loving you." },
+    { author: "avrill.v", quotes: "Whatever happens, keep breathing and still loving me mwehehehe :D" },
+    { author: "antjedtta_", quotes: "Happy\n Birthday I Wish The Best For you" },
+    { author: "standinshd", quotes: "Hey! Selamat Ulang Tahun Yang buka web ini heyyyy :v" },
+    { author: "standinshd", quotes: "Barakallahu fii umrik Sayang,Semoga selalu diberi kesehatan, selalu diberi umur yang berkah dan sukses di dunia maupun di akhirat. Aamiin" },
+    { author: "standinshd", quotes: "Semoga Apa Yang Disemogakan Tersemogakan" },
+    { author: "standinshd", quotes: "I love you :*" },
+    { author: "standinshd", quotes: "Happy Birthday For U Babe :*" },
 ];
