20c075f9d7d89d969ca21ec64d5d836a11c4fc11
adesahrull <64809307+adesahrull@users.noreply.github.com>
2020-05-05 04:11:43 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing and still loving me mwehehehe :D" },
-    { author: "antjedtta_", quotes: "Happy\n Birthday I Wish The Best For you" },
-    { author: "standinshd", quotes: "Hey! Selamat Ulang Tahun Yang buka web ini heyyyy :v" },
-    { author: "standinshd", quotes: "Barakallahu fii umrik Sayang,Semoga selalu diberi kesehatan, selalu diberi umur yang berkah dan sukses di dunia maupun di akhirat. Aamiin" },
-    { author: "standinshd", quotes: "Semoga Apa Yang Disemogakan Tersemogakan" },
-    { author: "standinshd", quotes: "I love you :*" },
-    { author: "standinshd", quotes: "Happy Birthday For U Babe :*" },
+    { author: "Sahrull Juliansyah", quotes: "Whatever happens, keep breathing and still loving me mwehehehe :D" },
+    { author: "Sahrull Juliansyah", quotes: "Happy\n Birthday I Wish The Best For you" },
+    { author: "Sahrull Juliansyah", quotes: "Hey! Selamat Ulang Tahun Yang buka web ini heyyyy :v" },
+    { author: "Sahrull Juliansyah", quotes: "Barakallahu fii umrik Sayang,Semoga selalu diberi kesehatan, selalu diberi umur yang berkah dan sukses di dunia maupun di akhirat. Aamiin" },
+    { author: "Sahrull Juliansyah", quotes: "Semoga Apa Yang Disemogakan Tersemogakan" },
+    { author: "Sahrull Juliansyah", quotes: "I love you :*" },
+    { author: "Sahrull Juliansyah", quotes: "Happy Birthday For U Babe :*" },
 ];
