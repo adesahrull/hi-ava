434bb606951cc592cc2134ef1af86d1dad9d9a61
adesahrull <64809307+adesahrull@users.noreply.github.com>
2020-05-07 22:15:00 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,4 @@
 // prettier-ignore
 export default [
-    { author: "Sahrull Juliansyah", quotes: "Whatever happens, keep breathing and still loving me mwehehehe :D" },
-    { author: "Sahrull Juliansyah", quotes: "Happy\n Birthday I Wish The Best For you" },
-    { author: "Sahrull Juliansyah", quotes: "Hey! Selamat Ulang Tahun Yang buka web ini heyyyy :v" },
-    { author: "Sahrull Juliansyah", quotes: "Barakallahu fii umrik Sayang,Semoga selalu diberi kesehatan, selalu diberi umur yang berkah dan sukses di dunia maupun di akhirat. Aamiin" },
-    { author: "Sahrull Juliansyah", quotes: "Semoga Apa Yang Disemogakan Tersemogakan" },
-    { author: "Sahrull Juliansyah", quotes: "I love you :*" },
-    { author: "Sahrull Juliansyah", quotes: "Happy Birthday For U Babe :*" },
+    { author: "Sahrull Juliansyah", quotes: "Happy Birthday For U Lop U :*" },
 ];
